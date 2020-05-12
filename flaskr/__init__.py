cee5a3867364d99b4a85f9422d1632b87653ecbb
uchchhwash <uchchhwash.git@gmail.com>
2020-05-12 18:31:38 +0600

    delete_question api completed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -83,6 +83,27 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+      
+      question.delete()
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+
+      return jsonify({
+        "success": True,
+        "deleted": question_id,
+        "questions": current_questions,
+        "total_questions": len(Question.query.all())
+      })
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -95,6 +116,31 @@ def create_app(test_config=None):
   of the questions list in the "List" tab.  
   '''
 
+  @app.route('/questions',methods=['POST'])
+  def add_question():
+    body = request.get_json()
+
+    new_question =  body.get('questions', None)
+    new_answer = body.get('answer', None)
+    new_category = body.get('category', None)
+    new_difficulty = body.get('difficulty', None)
+
+    try:
+      question = Question(question=new_question, answer=new_answer, category= new_category, difficulty=new_difficulty) 
+      question.insert()
+
+      selection = Question.query.order_by(Question.id).all()
+      current_page_questions = paginate_questions(request, selection)
+
+      return jsonify({
+        "success": True,
+        "created": question.id,
+        "questions": current_page_questions,
+        "total_quesitons": len(Question.query.all())
+      })
+
+    except:
+      abort(422) 
   '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
